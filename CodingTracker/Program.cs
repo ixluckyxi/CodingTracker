841d05d37a4e67b4b847583b3bfe960ded2fe6c0
ixluckyxi <jonathan.newsome@icloud.com>
2024-06-19 16:07:06 -0500

    Keeping one message

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification");
-Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification");
-
-
-
-
-
+Console.WriteLine("Hello, C# World! One more modification"); 
