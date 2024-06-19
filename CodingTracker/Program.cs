3a23cd5b7091448228603916b6543c3c310d137d
ixluckyxi <jonathan.newsome@icloud.com>
2024-06-19 16:10:49 -0500

    Revert "Keeping one message"
    This reverts commit 841d05d37a4e67b4b847583b3bfe960ded2fe6c0.

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# World! One more modification"); 
+Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification");
+Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification"); Console.WriteLine("Hello, C# World! One more modification");
+
+
+
+
+
